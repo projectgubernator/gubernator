37d6b90ef4ad373fa1ac3b842b6da94af30817d5
Samuel Roth <contact@samuelroth.net>
2021-08-01 10:11:10 -0400

    meh

--- gubernator_cri/src/lib.rs
+++ gubernator_cri/src/lib.rs
@@ -4,36 +4,3 @@ pub use generated::*;
 pub use generated::{
     image_service_client::ImageServiceClient, runtime_service_client::RuntimeServiceClient,
 };
-
-#[cfg(test)]
-mod tests {
-    #[tokio::test]
-    async fn image_service_client() {
-        let mut client = crate::ImageServiceClient::connect("http://localhost:548")
-            .await
-            .expect("failed to connect to mock image service");
-
-        let response = client
-            .list_images(crate::ListImagesRequest { filter: None })
-            .await
-            .expect("ListImagesRequest failed");
-
-        println!("{:?}", response);
-    }
-
-    #[tokio::test]
-    async fn runtime_service_client() {
-        let mut client = crate::RuntimeServiceClient::connect("http://localhost:549")
-            .await
-            .expect("failed to connect to mock runtime service");
-
-        let response = client
-            .version(crate::VersionRequest {
-                version: String::from("test"),
-            })
-            .await
-            .expect("ListImagesRequest failed");
-
-        println!("{:?}", response);
-    }
-}
