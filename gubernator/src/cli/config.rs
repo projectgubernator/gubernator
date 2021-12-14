57d0bc258658286fd5831ecd8cf073b86b0b6209
Paul Sorensen <paulsorensen5@gmail.com>
2021-12-14 18:46:34 -0500

    Replace SubCommand `about` call with `help` call (#19)

--- gubernator/src/cli/config.rs
+++ gubernator/src/cli/config.rs
@@ -4,6 +4,6 @@ pub fn subcommand<'a>() -> App<'a> {
     return App::new("config").about("Manage Gubernator config").arg(
         Arg::new("debug")
             .short('d')
-            .about("print debug information verbosely"),
+            .help("print debug information verbosely"),
     );
 }
