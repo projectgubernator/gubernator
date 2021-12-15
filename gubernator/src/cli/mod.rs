ba1cd693089c9cf977da76294a88fce97e9217e5
Paul Sorensen <paulsorensen5@gmail.com>
2021-12-14 23:16:33 -0500

    Fix typo in author email address (#20)

--- gubernator/src/cli/mod.rs
+++ gubernator/src/cli/mod.rs
@@ -6,7 +6,7 @@ mod config;
 fn main() {
     let _matches = App::new("Gubernator")
         .version("0.1.0")
-        .author("Samuel Roth <contact@@samuelroth.net>")
+        .author("Samuel Roth <contact@samuelroth.net>")
         .about("Interact with Gubernator clusters")
         .subcommand(apply::subcommand())
         .subcommand(config::subcommand())
